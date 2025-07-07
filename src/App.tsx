17cf020a7c8770df4c7310fa8cbb45ff5ba32652
yintii <keleheart1@gmail.com>
2025-07-07 10:48:33 -0700

    troublshooting deployment

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ import { RootState } from './state/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { setOverallScale, setFixedWallScale } from './state/menu/menuSlice';
 import man from './assets/man.svg';
-import { 
-	scaleArea,
-  roundToFixedUp
-} from './lib/helpers';
-
+//@ts-expect-error jbc
+import { scaleArea, roundToFixedUp } from './lib/helpers';
 
 
 export interface FixedWallProps {

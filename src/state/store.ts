8f73ed92c858a3f3a16c78e2db5fcf556b7bca14
yintii <keleheart1@gmail.com>
2025-09-29 13:07:48 -0700

    resolving a TS4023

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,12 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import menuReducer from './menu/menuSlice';
 import moduleMenuReducer from './moduleMenu/moduleMenuSlice';
 
-export const store = configureStore({
+export const store: any = configureStore({
     reducer: {
         menu: menuReducer,
         moduleMenu: moduleMenuReducer,
     },
-})
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
